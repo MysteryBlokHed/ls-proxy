3e17256a54619629153cf9570c739ed37682fa84
Adam Thompson-Sharpe <adamthompsonsharpe@gmail.com>
2022-02-13 08:33:50 -0500

    Update runtime-type-checking.ts example

--- examples/runtime-type-checking.ts
+++ examples/runtime-type-checking.ts
@@ -1,41 +1,32 @@
-import { storeObject, validateKeys } from 'ls-proxy'
+import { storeObject, validateKeys, validateTypes } from 'ls-proxy'
 
 interface Person {
   name: string
   age: number
 }
 const personKeys = ['name', 'age']
-
-/** Validate that an entry is a Person */
-const validate = (value: any): boolean => {
-  // Check that the only keys on the passed object are keys of Person
-  if (!validateKeys(value, personKeys)) return false
-  // Check that keys are of the correct types
-  if (typeof value.name !== 'string') return false
-  if (typeof value.age !== 'number') return false
-  return true
+const typesMap = {
+  name: 'string',
+  age: 'number',
 }
 
-const myObject = storeObject(
+const myObject = storeObject<Person>(
   'myObject',
   {
-    people: [
-      {
-        name: 'John',
-        age: 21,
-      },
-      {
-        name: 'Marvin',
-        age: 42,
-      },
-    ] as Person[],
+    name: 'Joe',
+    age: 20,
   },
-  { validate },
+  {
+    validate: value =>
+      validateKeys(value, personKeys) && validateTypes(value, typesMap),
+  },
 )
 
 // No error
-myObject.people[0].name = 'Joe'
-myObject.people[0].age = 20
+myObject.name = 'Marvin'
+myObject.age = 42
 
-// Causes TypeScript error and throws error at runtime
-myObject.people[1].name = 42
+// Runtime error due to invalid type
+myObject.name = 123
+// Runtime error due to unknown key
+myObject.abc = true
