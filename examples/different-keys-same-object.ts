10ae9ecc835bf646727006d5c3afd5c2d854764a
Adam Thompson-Sharpe <adamthompsonsharpe@gmail.com>
2022-02-14 16:04:09 -0500

    Add example using partial

---
+++ examples/different-keys-same-object.ts
@@ -0,0 +1,14 @@
+// Using two objects to watch different keys on the same object in localStorage
+import { storeObject } from 'ls-proxy'
+
+const sameKey = 'sameKey'
+
+const obj1 = storeObject(sameKey, { foo: 1 }, { partial: true })
+const obj2 = storeObject(sameKey, { bar: 2 }, { partial: true })
+
+console.log(obj1.foo) // 1
+console.log(obj2.bar) // 2
+
+// The only values on each object are the ones passed
+console.log(Object.entries(obj1)) // [['foo', 1]]
+console.log(Object.entries(obj2)) // [['bar', 2]]
