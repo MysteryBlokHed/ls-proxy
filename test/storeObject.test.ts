28b465d71cd95f043ee7f9290a1321e2de3029de
Adam Thompson-Sharpe <adamthompsonsharpe@gmail.com>
2022-02-19 20:14:50 -0500

    Add tests for nested objects

--- test/storeObject.test.ts
+++ test/storeObject.test.ts
@@ -28,4 +28,48 @@ describe('storeObject function', () => {
     localStorage.myObj = JSON.stringify({ foo: 'baz' })
     expect(myObj.foo).toBe('baz')
   })
+
+  it('properly writes nested objects', () => {
+    const myObj = storeObject('myObj', { nested: { foo: 'bar' } })
+    expect(localStorage.myObj).toBe(JSON.stringify({ nested: { foo: 'bar' } }))
+    myObj.nested.foo = 'baz'
+    expect(localStorage.myObj).toBe(JSON.stringify({ nested: { foo: 'baz' } }))
+  })
+
+  it('properly reads nested objects', () => {
+    const myObj = storeObject('myObj', { nested: { foo: 'bar' } })
+    localStorage.myObj = JSON.stringify({ nested: { foo: 'baz' } })
+    expect(myObj.nested.foo).toBe('baz')
+  })
+
+  it('properly writes deeply nested objects', () => {
+    const deeplyNested = {
+      nested1: {
+        nested2: {
+          foo: 'bar',
+        },
+      },
+    }
+    const myObj = storeObject('myObj', deeplyNested)
+    expect(localStorage.myObj).toBe(JSON.stringify(deeplyNested))
+    myObj.nested1.nested2.foo = 'baz'
+    expect(JSON.parse(localStorage.myObj).nested1?.nested2?.foo).toBe('baz')
+  })
+
+  it('properly reads deeply nested objects', () => {
+    const deeplyNested = {
+      nested1: {
+        nested2: {
+          foo: 'bar',
+        },
+      },
+    }
+    const myObj = storeObject('myObj', deeplyNested)
+
+    const modified = { ...deeplyNested }
+    modified.nested1.nested2.foo = 'baz'
+
+    localStorage.myObj = JSON.stringify(modified)
+    expect(myObj.nested1.nested2.foo).toBe('baz')
+  })
 })
