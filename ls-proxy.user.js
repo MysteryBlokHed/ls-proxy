7acd23528da841b03628010b43e89bac59aa9fc8
Adam Thompson-Sharpe <adamthompsonsharpe@gmail.com>
2022-03-10 18:48:14 -0500

    :bookmark: Release v0.4.0

--- ls-proxy.user.js
+++ ls-proxy.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name        ls-proxy
 // @descripton  Wrapper around localStorage to easily store JSON objects
-// @version     0.3.1
+// @version     0.4.0
 // @author      Adam Thompson-Sharpe
 // @license     MIT OR Apache-2.0
 // @homepageURL https://gitlab.com/MysteryBlokHed/ls-proxy
