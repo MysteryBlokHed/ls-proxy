d0d89027c4078fb7c33a62b2e8cf07802c419d70
Adam Thompson-Sharpe <adamthompsonsharpe@gmail.com>
2022-03-11 17:53:16 -0500

    Update JSDoc examples

--- src/index.ts
+++ src/index.ts
@@ -200,19 +200,19 @@ const nestedProxyHandler = <
  *   minor: boolean
  * }
  *
- * const myPerson = storeObject(
+ * const myPerson = storeObject<Person>(
  *   'myPerson',
  *   {
  *     name: 'Ellie',
  *     age: 17,
  *     minor: true,
- *   } as Person,
+ *   },
  *   {
  *     // If the person's age is 18 or greater, set minor to false.
  *     // Otherwise, set it to true.
  *     // This will affect values as they're being stored in localStorage
  *     // and retrieved from it
- *     validate(value) {
+ *     modify(value) {
  *       if (value.age >= 18) value.minor = false
  *       else value.minor = true
  *       return value
