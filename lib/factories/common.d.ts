d9bfff04d49ea035ab7f6077aafed20f4e57323d
Adam Thompson-Sharpe <adamthompsonsharpe@gmail.com>
2022-11-07 10:47:51 -0500

    feat(factories): Move common functions for React-like state to new file
    Since SolidJS has state management very similar to react, it'd be weird
    to copy+paste the entire `storeStateful` function for React and just
    change a couple of lines. This change creates a more generic function
    that can be called with some library-specific configuration.

---
+++ lib/factories/common.d.ts
@@ -0,0 +1,25 @@
+import { StoreSeparateConfig } from '..';
+import type { Keys } from '../types';
+/** Basic signature for setState function */
+export declare type SetState<T> = (value: T) => void;
+/** An object that contains setState functions matching the types of the provided object */
+export declare type SetStateFunctions<O extends Record<string, any>> = {
+    [K in keyof O]: SetState<O[K]>;
+};
+export declare function keyInObject<O extends Record<string, any>>(key: string, object: O): asserts key is Keys<O>;
+export interface StoreReactlikeStateOptions<O extends Record<string, any>> {
+    /**
+     * Should handle getting defaults from the useState function
+     * @param defaults The defaults values
+     * @returns The current values of each key as well as their setState functions
+     */
+    setDefaults(defaults: Readonly<O>): {
+        current: O;
+        stateFunctions: SetStateFunctions<O>;
+    };
+}
+export declare type Options<O extends Record<string, any>> = Omit<StoreSeparateConfig<O>, 'checkGets' | 'checkDefaults' | 'parse' | 'set' | 'stringify'>;
+/**
+ * @param defaults The defaults values if they are undefined
+ */
+export declare function storeReactlikeState<O extends Record<string, any>>(defaults: O, reactlikeOptions: StoreReactlikeStateOptions<O>, passedConfiguration: Options<O>, overrideConfiguration?: StoreSeparateConfig<O>): O;
